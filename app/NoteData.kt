450927875cd84aab9b1c86623d20255291f24b16
DeokGyuYoun <106170934+DeokGyuYoun@users.noreply.github.com>
2022-10-25 17:00:36 +0900

    20221025

---
+++ app/NoteData.kt
@@ -0,0 +1,12 @@
+package com.example.mymemo
+
+class NoteData {
+    private var String subject
+    private var String body
+
+    public NoteData(String subject, String body){
+        this.subject = subject
+        this.body = body
+    }
+
+}
