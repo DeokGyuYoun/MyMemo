c90b52590b0cd7cd5081f6f59026a6fc576c913c
DeokGyuYoun <106170934+DeokGyuYoun@users.noreply.github.com>
2022-11-03 17:00:09 +0900

    20221103

--- app/src/main/java/com/example/mymemo/MainActivity.kt
+++ app/src/main/java/com/example/mymemo/MainActivity.kt
@@ -1,44 +1,59 @@
 package com.example.mymemo
 
-import android.content.Intent
+import android.animation.ObjectAnimator
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.mymemo.databinding.ActivityMainBinding
+import android.content.Intent
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
-import com.example.mymemo.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private var isFabOpen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        // 플로팅 버튼 클릭시 에니메이션 동작 기능
+        fabMain.setOnClickListener {
+            toggleFab()
+        }
 
-        setSupportActionBar(binding.toolbar)
+        // 플로팅 버튼 클릭 이벤트 - 카메라
+        fabCamera.setOnClickListener {
+            Toast.makeText(this, "카메라 버튼 클릭!", Toast.LENGTH_SHORT).show()
+        }
 
-        binding.fab.setOnClickListener { view ->
-            val intent = Intent(this@MainActivity, NoteActivity::class.java)
-            startActivity(intent)
+        // 플로팅 버튼 클릭 이벤트 - 수정
+        fabEdit.setOnClickListener {
+            Toast.makeText(this, "수정 버튼 클릭!", Toast.LENGTH_SHORT).show()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    /***
+     *  플로팅 액션 버튼 클릭시 동작하는 애니메이션 효과 세팅
+     */
+    private fun toggleFab() {
+        Toast.makeText(this, "메인 플로팅 버튼 클릭 : $isFabOpen", Toast.LENGTH_SHORT).show()
+
+        // 플로팅 액션 버튼 닫기 - 열려있는 플로팅 버튼 집어넣는 애니메이션 세팅
+        if (isFabOpen) {
+            ObjectAnimator.ofFloat(fabCamera, "translationY", 0f).apply { start() }
+            ObjectAnimator.ofFloat(fabEdit, "translationY", 0f).apply { start() }
+            fabMain.setImageResource(R.drawable.ic_baseline_add_24)
+
+            // 플로팅 액션 버튼 열기 - 닫혀있는 플로팅 버튼 꺼내는 애니메이션 세팅
+        } else {
+            ObjectAnimator.ofFloat(fabCamera, "translationY", -200f).apply { start() }
+            ObjectAnimator.ofFloat(fabEdit, "translationY", -400f).apply { start() }
+            fabMain.setImageResource(R.drawable.ic_baseline_clear_24)
         }
+
+        isFabOpen = !isFabOpen
+
     }
 }
---
+++ app/src/main/java/com/example/mymemo/NoteData.kt
@@ -0,0 +1,11 @@
+package com.example.mymemo
+
+class NoteData {
+    private var subject:String = ""
+    private var body:String = ""
+
+    public fun NoteData(subject:String, body:String) {
+        this.subject = subject
+        this.body = body
+    }
+}
